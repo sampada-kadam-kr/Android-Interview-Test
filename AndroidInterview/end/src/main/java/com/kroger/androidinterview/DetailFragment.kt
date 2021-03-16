308665a992cdd67300b8531a7de1ba115da54669
Sampada Kadam <sampada.kadam2@kroger.com>
2021-03-16 12:53:13 -0400

    added entity/db/dao

--- AndroidInterview/end/src/main/java/com/kroger/androidinterview/DetailFragment.kt
+++ AndroidInterview/end/src/main/java/com/kroger/androidinterview/DetailFragment.kt
@@ -8,24 +8,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+
+class DetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 }
---
+++ AndroidInterview/end/src/main/java/com/kroger/androidinterview/db/ComicsDatabase.kt
@@ -0,0 +1,7 @@
+package com.kroger.androidinterview.db
+
+import androidx.room.RoomDatabase
+
+public abstract class ComicsDatabase : RoomDatabase(){
+
+}
