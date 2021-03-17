cd7e2e55d3e3704387697f34407ade017b25d1af
Sampada Kadam <sampada.kadam2@kroger.com>
2021-03-17 14:42:14 -0400

    main activity

--- AndroidInterview/app/src/main/java/com/kroger/start/MainActivity.kt
+++ AndroidInterview/app/src/main/java/com/kroger/start/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
