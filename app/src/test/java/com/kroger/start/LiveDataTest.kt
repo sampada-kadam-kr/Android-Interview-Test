ff170cc78029712b03044910b30a39b421777f78
Ethan Berg <ethan.berg@kroger.com>
2023-02-10 13:44:41 -0800

    Intant task execution in Junit 5

--- app/src/test/java/com/kroger/start/LiveDataTest.kt
+++ app/src/test/java/com/kroger/start/LiveDataTest.kt
@@ -3,10 +3,12 @@ package com.kroger.start
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
 /**
  * This is just a test that these classes are available on the classpath
  */
+@ExtendWith(InstantExecutorExtension::class)
 class LiveDataTest {
 
     @Test
