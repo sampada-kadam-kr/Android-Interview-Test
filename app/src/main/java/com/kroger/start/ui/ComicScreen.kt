811526e04facf83f86dea244eda887e41d7cd643
Ethan Berg <ethan.berg@kroger.com>
2023-02-10 14:00:26 -0800

    added preview for compose screen

--- app/src/main/java/com/kroger/start/ui/ComicScreen.kt
+++ app/src/main/java/com/kroger/start/ui/ComicScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kroger.start.R
 
@@ -25,11 +27,21 @@ fun ComicScreen() {
                 Text(
                     modifier = Modifier.padding(start = 16.dp),
                     text = stringResource(id = R.string.app_name),
-                    style = MaterialTheme.typography.h1,
+                    style = MaterialTheme.typography.h6,
                 )
             }
         },
     ) {
         // Content
+    }
+}
+
+@Preview(
+    device = Devices.PHONE,
+)
+@Composable
+private fun ComicScreenPreview() {
+    MaterialTheme {
+        ComicScreen()
     }
 }
